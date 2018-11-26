e7bc1035d582cbd24e613da9edbce4508b64e944
Simin Shamsfallah <Simin.Shamsfallah@Earlens.com>
2018-11-25 23:07:17 -0800

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 //Set up mysql connection
 var mysql = require ("mysql");
+var connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+    connection = mysql.createConnection({
+        host:"localhost",
+        port:3306,
+        user:"root",
+        password:"SHams1363$",
+        database:"burger_db"
+        });
+}
 
-var connection = mysql.createConnection({
-host:"localhost",
-poryt:3306,
-user:"root",
-password:"SHams1363$",
-database:"burger_db"
-});
 //Make connection
 connection.connect(function(err){
     if(err){
